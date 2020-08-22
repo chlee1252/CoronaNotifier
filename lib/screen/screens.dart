ffc2333f43d5afc0c55504ed322ed652a52d1ec0
Marc Lee <chlee1252@gmail.com>
2020-08-22 13:16:46 +0900

    export screens

---
+++ lib/screen/screens.dart
@@ -0,0 +1,4 @@
+export 'package:CoronaNotifier/screen/loadingScreen.dart';
+export 'package:CoronaNotifier/screen/infoScreen.dart';
+export 'package:CoronaNotifier/screen/detailScreen.dart';
+export 'package:CoronaNotifier/screen/chartScreen.dart';
